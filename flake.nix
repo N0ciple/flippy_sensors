b511c02f5772eab21b586e9e9d34f620c5a84698
Robin Dupont <robin.dpnt@gmail.com>
2024-11-26 14:19:52 +0100

    add alias for ufbt to uvx ufbt

--- flake.nix
+++ flake.nix
@@ -17,10 +17,9 @@
             python3
             python3Packages.uv
           ];
-          # shellHook = ''
-          # uv tool install ufbt
-          # '';
-          # --> run uvx ufbt instead
+          shellHook = ''
+          alias ufbt="uvx ufbt"
+          '';
         };
       });
 }
